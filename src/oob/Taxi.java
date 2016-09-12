a48135a82a2cb31442314c39e1cfae2019acc88a
jukai.zhang <jukai.zhang@rea-group.com>
2016-09-12 19:40:21 +0800

    add logic when distence is 1, 2, 3

--- src/oob/Taxi.java
+++ src/oob/Taxi.java
@@ -5,7 +5,15 @@ package oob;
  */
 public class Taxi {
 
-    public int count(int miles){
-        return 0;
+    public double count(int miles){
+        final int INITIAL_PRICE = 6;
+        double result;
+        if(miles >2){
+            result = INITIAL_PRICE + (miles-2) * 0.8;
+        }else {
+            result = (miles +1)/2 * INITIAL_PRICE;
+        }
+
+        return result;
     }
 }
--- test/taxiTest.java
+++ test/taxiTest.java
@@ -3,15 +3,37 @@
  */
 import oob.Taxi;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class taxiTest {
+    Taxi taxi;
+    final double PRECISION = 0.0001;
+    @Before
+    public void initialize() {
+        taxi  = new Taxi();
+    }
     @Test
-    public void testTaxi(){
-        Taxi taxi = new Taxi();
-        assertEquals(0,taxi.count(0));
+    public void testTaxiCount0(){
 
+        assertEquals(0,taxi.count(0), PRECISION);
+    }
+
+    @Test
+    public void testTaxiCount1(){
+        assertEquals(6,taxi.count(1), PRECISION);
+    }
+
+    @Test
+    public void testTaxiCount2(){
+
+        assertEquals(6,taxi.count(2), PRECISION);
+    }
+
+    @Test
+    public void testTaxiCount3(){
+        assertEquals(6.8,taxi.count(3), PRECISION);
     }
 }
