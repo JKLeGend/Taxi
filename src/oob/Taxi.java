c6e01d3adbb108133d92432b9d9ab0ca08073b3b
jukai.zhang <jukai.zhang@rea-group.com>
2016-09-25 10:47:42 +0800

    add logic for >8, and waiting time

--- src/oob/Taxi.java
+++ src/oob/Taxi.java
@@ -4,16 +4,41 @@ package oob;
  * Created by jukzhang on 9/12/16.
  */
 public class Taxi {
+    final int INITIAL_DISTANCE = 2;
+    final int NORMAL_DISTENCE = 8;
+    final int INITIAL_PRICE = 6;
+    final double UNIT_PRICE = 0.8;
+    final double EXTRA_PERCENT = 0.5;
+    final double UNIT_PRICE_OF_WAITING = 0.25;
 
-    public double count(int miles){
-        final int INITIAL_PRICE = 6;
+    public double counter(double miles, int waitingTime) {
+        if (miles == 0) return 0;
+
         double result;
-        if(miles >2){
-            result = INITIAL_PRICE + (miles-2) * 0.8;
-        }else {
-            result = (miles +1)/2 * INITIAL_PRICE;
+        if (miles > NORMAL_DISTENCE) {
+            result = INITIAL_PRICE + this.basicFeeCounter(miles) + this.extraFeeCounter(miles);
+        } else if (miles > INITIAL_DISTANCE) {
+            result = INITIAL_PRICE + this.basicFeeCounter(miles);
+        } else {
+            result = (miles + 1) / INITIAL_DISTANCE * INITIAL_PRICE;
         }
 
-        return result;
+        return this.priceRounder(result + this.timeCharger(waitingTime));
+    }
+
+    public double basicFeeCounter(double miles) {
+        return (miles - INITIAL_DISTANCE) * UNIT_PRICE;
+    }
+
+    public double extraFeeCounter(double miles) {
+        return (miles - NORMAL_DISTENCE) * UNIT_PRICE * EXTRA_PERCENT;
+    }
+
+    public double timeCharger(double waitingTime) {
+        return waitingTime * UNIT_PRICE_OF_WAITING;
+    }
+
+    public int priceRounder(double price) {
+        return (int) Math.round(price);
     }
 }
--- test/taxiTest.java
+++ test/taxiTest.java
@@ -1,8 +1,8 @@
 /**
  * Created by jukzhang on 9/12/16.
  */
+
 import oob.Taxi;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -11,29 +11,78 @@ import static org.junit.Assert.assertEquals;
 public class taxiTest {
     Taxi taxi;
     final double PRECISION = 0.0001;
+
     @Before
     public void initialize() {
-        taxi  = new Taxi();
-    }
-    @Test
-    public void testTaxiCount0(){
-
-        assertEquals(0,taxi.count(0), PRECISION);
+        taxi = new Taxi();
     }
 
     @Test
-    public void testTaxiCount1(){
-        assertEquals(6,taxi.count(1), PRECISION);
+    public void testTaxiCounterWithMiles0() {
+        assertEquals(0, taxi.counter(0, 0), PRECISION);
+        assertEquals(0, taxi.counter(0, 4), PRECISION);
     }
 
     @Test
-    public void testTaxiCount2(){
-
-        assertEquals(6,taxi.count(2), PRECISION);
+    public void testTaxiCounterWithMiles1() {
+        assertEquals(6, taxi.counter(1, 0), PRECISION);
+        assertEquals(7, taxi.counter(1, 4), PRECISION);
     }
 
     @Test
-    public void testTaxiCount3(){
-        assertEquals(6.8,taxi.count(3), PRECISION);
+    public void testTaxiCounterWithMiles2() {
+        assertEquals(6, taxi.counter(2, 0), PRECISION);
+        assertEquals(7, taxi.counter(2, 4), PRECISION);
+    }
+
+    @Test
+    public void testTaxiCounterWithMiles3() {
+        assertEquals(7, taxi.counter(3, 0), PRECISION);
+        assertEquals(8, taxi.counter(3, 4), PRECISION);
+    }
+
+    @Test
+    public void testTaxiCounterWithMiles5() {
+        assertEquals(8, taxi.counter(5, 0), PRECISION);
+        assertEquals(9, taxi.counter(5, 4), PRECISION);
+    }
+
+    @Test
+    public void testTaxiCounterWithMiles8() {
+        assertEquals(11, taxi.counter(8, 0), PRECISION);
+        assertEquals(12, taxi.counter(8, 4), PRECISION);
+    }
+
+    @Test
+    public void testTaxiCounterWithMiles9() {
+        assertEquals(12, taxi.counter(9, 0), PRECISION);
+        assertEquals(13, taxi.counter(9, 4), PRECISION);
+    }
+
+    @Test
+    public void testTaxiCounterWithMiles99() {
+        assertEquals(120, taxi.counter(99, 0), PRECISION);
+        assertEquals(121, taxi.counter(99, 4), PRECISION);
+    }
+
+    @Test
+    public void testTaxiBasicFeeCounter() {
+        assertEquals(1.6, taxi.basicFeeCounter(4), PRECISION);
+    }
+
+    @Test
+    public void testTaxiExtraFeeCounter() {
+        assertEquals(0.8, taxi.extraFeeCounter(10), PRECISION);
+    }
+
+    @Test
+    public void testTaxiTimeCharger() {
+        assertEquals(2, taxi.timeCharger(4), PRECISION);
+    }
+
+    @Test
+    public void testTaxiPriceCounter() {
+        assertEquals(1, taxi.priceRounder(1.2), PRECISION);
+        assertEquals(2, taxi.priceRounder(1.5), PRECISION);
     }
 }
